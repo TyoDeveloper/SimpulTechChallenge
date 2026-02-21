195c2af62d805b8436e076b131240e44eb4568ae
TyoDeveloper <developer.academy2025@gmail.com>
2026-02-22 00:49:04 +0700

    add animasi deskripsi

--- app/Components/Common/LoadingPanel.tsx
+++ app/Components/Common/LoadingPanel.tsx
@@ -7,12 +7,6 @@ type LoadingPanelProps = {
 export default function LoadingPanel({ text }: LoadingPanelProps) {
   return (
     <div className="w-[420px] h-[520px] bg-white rounded-2xl shadow-xl flex flex-col">
-      {/* HEADER PLACEHOLDER */}
-      <div className="flex justify-between items-center p-4 border-b">
-        <div className="w-28 h-8 bg-gray-200 rounded animate-pulse" />
-        <div className="w-20 h-8 bg-gray-200 rounded animate-pulse" />
-      </div>
-
       {/* BODY */}
       <div className="flex flex-col items-center justify-center flex-1">
         {/* Spinner */}
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="fixed top-0 left-0 w-full bg-blue-500 overflow-hidden h-12 flex items-center">
+          <div className="marquee text-white text-lg font-semibold">
+            Simpul Tech Challenge - Ketika Lighning icon disorot muncul Task Icon dan Inbox Icon,
+            ketika masing-masing di klik mengaktifkan mode nya masing-masing, ketika mode yang
+            sedang aktif di klik maka kembali ke mode awal{" "}
+          </div>
+        </div>
         <FloatingMenu />
       </main>
     </div>
